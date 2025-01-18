a179e0c7a3aaab9516797f6b8bbb9b8adb47e561
Guro <guro.stromnes@gmail.com>
2025-01-18 16:30:36 +0100

    Lagt inn funksjon for post for å finne sum av to parametere i url + lagt inn global sjekk for feilmelding 404.

--- script.mjs
+++ script.mjs
@@ -19,9 +19,27 @@ function getQuote(req, res, next) {
   res.status(HTTP_CODES.SUCCESS.OK).send(quotes[randomQuoteNumber]).end();
 }
 
+function getSum(req, res, next) {
+  const a = parseFloat(req.params.a);
+  const b = parseFloat(req.params.b);
+  const sum = a + b;
+
+  if(isNaN(a) || isNaN(b)) {
+    res.status(HTTP_CODES.CLIENT_ERROR.BAD_REQUEST).send("Invalid input. You need to provide two numbers.").end();
+  }
+
+  res.status(HTTP_CODES.SUCCESS.OK).send(`The sum of ${a} + ${b} = ${sum}`).end();
+}
+
 server.get("/", getRoot);
 server.get("/tmp/poem", getPoem);
 server.get("/tmp/quote", getQuote);
+
+server.post("/tmp/sum/:a/:b", getSum);
+
+server.use((req, res, next) => {
+    res.status(HTTP_CODES.CLIENT_ERROR.NOT_FOUND).send("Page not found.").end();
+});
 
 server.listen(server.get("port"), function () {
   console.log("server running", server.get("port"));
