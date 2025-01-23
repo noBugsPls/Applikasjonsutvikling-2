5e549b6c30e42b5630e65db907b141bb08ec9069
Guro <guro.stromnes@gmail.com>
2025-01-24 00:35:28 +0100

    Completed client implementation to interact with deck API and display cards

--- script.mjs
+++ script.mjs
@@ -5,8 +5,9 @@ const server = express();
 const port = process.env.PORT || 8000;
 
 server.set("port", port);
-server.use(express.static("public"));
+server.use(express.static("public", { extensions: ["html"] }));
 
+//==================================== Functions ====================================
 function getRoot(req, res, next) {
   res.status(HTTP_CODES.SUCCESS.OK).send("Hello World").end();
 }
@@ -31,12 +32,110 @@ function getSum(req, res, next) {
   res.status(HTTP_CODES.SUCCESS.OK).send(`The sum of ${a} + ${b} = ${sum}`).end();
 }
 
+function postDeck(req, res, next) {
+    const newDeck = createDeck();
+    decks[newDeck.deck_id] = newDeck;
+  res.status(HTTP_CODES.SUCCESS.OK).send({deck_id: newDeck.deck_id}).end();
+}
+
+function createDeck() {
+    const suits = [{name: "hearts", symbol: "♥"}, {name: "diamonds", symbol: "♦"}, {name: "clubs", symbol: "♣"}, {name: "spades", symbol: "♠"}];
+    const values = [{value: "1", name: "ace"}, {value: "2", name: "two"}, {value: "3", name: "three"}, {value: "4", name: "four"}, {value: "5", name: "five"}, {value: "6", name: "six"}, {value: "7", name: "seven"}, {value: "8", name: "eight"}, {value: "9", name: "nine"}, {value: "10", name: "ten"}, {value: "11", name: "jack"}, {value: "12", name: "queen"}, {value: "13", name: "king"}];
+
+    let deck_id;
+
+    if(decks.length === 0) {
+      deck_id = 1;
+    }else{
+      deck_id = decks.length;
+    }
+
+    const cards = [];
+
+    for(const suit of suits){
+        for(const value of values){
+            cards.push({suit: suit.symbol, value: value.value});
+        }
+    }
+    return {deck_id, cards, remaining: cards.length};
+}
+
+function getCard(req, res, next) {
+    const deck_id = req.params.deck_id;
+    const deck = decks[deck_id];
+
+    if(deck === undefined) {
+        res.status(HTTP_CODES.CLIENT_ERROR.NOT_FOUND).send("Deck not found.").end();
+    }
+
+    if(deck.remaining === 0) {
+        res.status(HTTP_CODES.CLIENT_ERROR.NOT_FOUND).send("No cards remaining.").end();
+    }
+
+    const randomCardIndex = Math.floor(Math.random() * deck.cards.length);
+
+    const card = deck.cards.splice(randomCardIndex, 1);
+    const drawnCard = card[0];
+    deck.remaining = deck.cards.length;
+
+    res.status(HTTP_CODES.SUCCESS.OK).json({drawnCard, remaining: deck.remaining}).end();
+}
+
+function shuffleDeck(req, res, next) {
+    const deck_id = req.params.deck_id;
+    const deck = decks[deck_id];
+
+    if(deck === undefined) {
+        res.status(HTTP_CODES.CLIENT_ERROR.NOT_FOUND).send("Deck not found.").end();
+    }
+
+    deck.cards.sort(() => Math.random() - 0.5);
+
+    res.status(HTTP_CODES.SUCCESS.OK).json({shuffled: "true"}).end();
+}
+
+function showDeck(req, res, next) {
+    const deck_id = req.params.deck_id;
+    const deck = decks[deck_id];
+
+    if (!deck) {
+        return res.status(HTTP_CODES.CLIENT_ERROR.NOT_FOUND).send("Deck not found.");
+    }
+
+    // Sjekk om deck.cards er et array
+    if (!Array.isArray(deck.cards)) {
+        return res.status(HTTP_CODES.CLIENT_ERROR.BAD_REQUEST).send("Deck.cards is not valid.");
+    }
+
+    // Bygg en streng som representerer kortstokken
+    let deckString = [];
+    for (const card of deck.cards) {
+        deckString.push(`value: ${card.value}, suit: ${card.suit}`);
+    }
+
+    // Send kortstokken som svar
+    res.status(HTTP_CODES.SUCCESS.OK).json(deckString);
+}
+
+//http://localhost:8000/temp/shuffle/2
+//http://localhost:8000/temp/deck
+
+//==================================== Routes ====================================
+//------------------------ GET ------------------------
 server.get("/", getRoot);
 server.get("/tmp/poem", getPoem);
 server.get("/tmp/quote", getQuote);
+server.get("/temp/deck/:deck_id/card", getCard);
+server.get("/temp/deck/:deck_id", showDeck);
 
+//------------------------ POST ------------------------
 server.post("/tmp/sum/:a/:b", getSum);
+server.post("/temp/deck", postDeck);
 
+//------------------------ PATCH ------------------------
+server.patch("/temp/deck/shuffle/:deck_id", shuffleDeck);
+
+//------------------------ 404 error-code ------------------------
 server.use((req, res, next) => {
     res.status(HTTP_CODES.CLIENT_ERROR.NOT_FOUND).send("Page not found.").end();
 });
@@ -44,6 +143,10 @@ server.use((req, res, next) => {
 server.listen(server.get("port"), function () {
   console.log("server running", server.get("port"));
 });
+
+//==================================== Data ====================================
+
+const decks = [];
 
 const poem = `<pre>
 Jeg våknet en natt av en underlig drøm
