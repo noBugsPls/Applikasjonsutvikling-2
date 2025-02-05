21ce0df441fe7b5bd75146b8f7b1bf0f77bd537c
Guro <guro.stromnes@gmail.com>
2025-02-05 15:47:13 +0100

    Lagt inn all kode fra forelesning

--- server.mjs
+++ server.mjs
@@ -1,19 +1,26 @@
 import express from "express";
 import HTTP_CODES from "./utils/httpCodes.mjs";
+import log from "./modules/log.mjs";
+import { LOGG_LEVELS, eventLogger } from "./modules/log.mjs";
 
 import sumRoutes from "./routes/sumRoutes.mjs";
 import poemRoutes from "./routes/poemRoutes.mjs";
 import deckRoutes from "./routes/deckRoutes.mjs";
 
+const ENABLE_LOGGING = false;
+
 const server = express();
 const port = process.env.PORT || 8000;
 
-server.set("port", port);
-server.use(express.static("public", { extensions: ["html"] }));
+const logger = log(LOGG_LEVELS.VERBOSE);
 
-server.use("", sumRoutes);
-server.use("", poemRoutes);
-server.use("", deckRoutes);
+server.set("port", port);
+server.use(logger)
+server.use(express.static("public"));
+
+server.use("", sumRoutes, logger);
+server.use("", poemRoutes, logger);
+server.use("", deckRoutes, logger);
 
 //------------------------ 404 error-code ------------------------
 server.use((req, res, next) => {
