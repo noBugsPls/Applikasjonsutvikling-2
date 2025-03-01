95433d1cebfbf138c9d8d01a719ed4343d3665a0
Guro <guro.stromnes@gmail.com>
2025-03-02 00:06:48 +0100

    Utført små feilrettinger for cache

--- public/app.js
+++ public/app.js
@@ -1,30 +1,29 @@
-
- if("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/sw.js");
-} 
-
-import aboutViewHTML from './views/aboutView.mjs';
-import patternsViewHTML from './views/patternsView.mjs';
-        import homeViewHTML from './views/homeView.mjs';
-
-
-        const routes = {
-            home: homeViewHTML,
-            patterns: patternsViewHTML,
-            about: aboutViewHTML,
-        };
-
-        async function navigate(route) {
-    const app = document.getElementById("app");
-    app.innerHTML = routes[route] || "<h2>Siden finnes ikke</h2>";
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/sw.js")
 }
 
-document.querySelectorAll("nav").forEach(link => {
-    link.addEventListener("click", event => {
-        event.preventDefault();
-        const route = event.target.dataset.route;
-        navigate(route);
-    });
+import aboutViewHTML from "./views/aboutView.mjs";
+import patternsViewHTML from "./views/patternsView.mjs";
+import homeViewHTML from "./views/homeView.mjs";
+
+const routes = {
+  home: homeViewHTML,
+  patterns: patternsViewHTML,
+  about: aboutViewHTML,
+};
+
+async function navigate(route) {
+  const app = document.getElementById("app");
+  app.innerHTML = routes[route] || "<h2>Siden finnes ikke</h2>";
+}
+
+document.querySelectorAll("nav").forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+    const route = event.target.dataset.route;
+    navigate(route);
+  });
 });
 
 navigate("home");
--- public/sw.js
+++ public/sw.js
@@ -1,57 +1,64 @@
-const cacheID= "KnittingPatternsV1";
+const cacheID = "KnittingPatternsV1";
 const contentToCache = [
-    "/index.html",
-    "/app.js",
-    "/icons/knitting_small.png",
-    "/icons/knitting_big.png",
-    "/css/style.css",
-]
+  "/index.html",
+  "/app.js",
+  "/icons/knitting_small.png",
+  "/icons/knitting_big.png",
+  "/css/style.css",
+];
 
-
-self.addEventListener('install', event => {
-    console.log('[Service Worker] Install');
-    event.waitUntil((async () => {
-        const cache = await caches.open(cacheID);
+self.addEventListener("install", (event) => {
+  console.log("[Service Worker] Install");
+  event.waitUntil(
+    (async () => {
+      
+      const cache = await caches.open(cacheID);
         console.log('[Service Worker] Caching all: app shell and content');
-        await cache.addAll(contentToCache);
-    })());
 
-    self.skipWaiting();
+        await cache.addAll(contentToCache); 
+    })()
+  );
+
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-    if (!(
-        event.request.url.startsWith('http:') || event.request.url.startsWith('https:')
-    )) {
-        return;
-    }
+self.addEventListener("fetch", (event) => {
+  if (!(event.request.url.startsWith("http:") || event.request.url.startsWith("https:"))) {
+    return;
+  }
 
-    event.respondWith((async () => {
-        const r = await caches.match(event.request);
+  event.respondWith(
+    (async () => {
+      const r = await caches.match(event.request);
         console.log(`[Service Worker] Fetching resource: ${event.request.url}`);
         if (r) { return r };
-        const response = await fetch(event.request);
-        const cache = await caches.open(cacheID);
-        console.log(`[Service Worker] Caching new resource: ${event.request.url}`);
-        cache.put(event.request, response.clone());
-        return response;
-    })());
+            const response = await fetch(event.request);
+            const cache = await caches.open(cacheID);
+            console.log(`[Service Worker] Caching new resource: ${event.request.url}`);
+            cache.put(event.request, response.clone());
+            return response; 
+    })()
+  );
 });
 
-self.addEventListener('activate', event => {
-    console.log('[Service Worker] Activate');
+self.addEventListener("activate", (event) => {
+  console.log("[Service Worker] Activate");
 
-    event.waitUntil((async () => {
-        const cacheNames = await caches.keys();
-        const cachesToDelete = cacheNames.filter(cacheName => cacheName !== cacheID);
+  event.waitUntil(
+    (async () => {
+       const cacheNames = await caches.keys();
+      const cachesToDelete = cacheNames.filter((cacheName) => cacheName !== cacheID);
 
-        await Promise.all(cachesToDelete.map(cacheName => {
-                console.log(`[Service Worker] Deleting cache: ${cacheName}`);
-                return caches.delete(cacheName);
-        }));
+      await Promise.all(
+        cachesToDelete.map((cacheName) => {
+          console.log(`[Service Worker] Deleting cache: ${cacheName}`);
+          return caches.delete(cacheName);
+        })
+      );
 
-        console.log(`[Service Worker] Keeping cache: ${cacheID}`);
+      console.log(`[Service Worker] Keeping cache: ${cacheID}`);
 
-        await self.clients.claim();
-    })());
+      await self.clients.claim(); 
+    })()
+  );
 });
