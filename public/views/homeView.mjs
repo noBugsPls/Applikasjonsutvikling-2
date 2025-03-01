28ddf5477ad97f3e2cb3b4985c66732df4d09145
Guro <guro.stromnes@gmail.com>
2025-03-01 15:26:50 +0100

    Lagt inn templates for ulike sider

---
+++ public/views/homeView.mjs
@@ -0,0 +1,8 @@
+const homeViewHTML = `
+<h1>Knitting Patterns</h1>
+<p>A place to keep all of your beloved knitting patterns.</p>
+<button id="newPattern">Create new pattern</button>
+<button id="showPatterns">Show patterns</button>
+`;
+
+export default homeViewHTML;
---
+++ public/views/patternsView.mjs
@@ -0,0 +1,7 @@
+
+const patternsViewHTML = `
+<h1>Knitting Patterns</h1>
+<p>No patterns yet.</p>
+`;
+
+export default patternsViewHTML;
