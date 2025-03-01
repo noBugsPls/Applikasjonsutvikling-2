6e7d542efa7c1103378b5b5dc715dd994cc42a5a
Guro <guro.stromnes@gmail.com>
2025-03-01 14:50:35 +0100

    Fjernet unødvendige kontroller og datafiler for å rydde opp i prosjektet

--- routes/sumRoutes.mjs
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getSum } from "../controllers/sumController.mjs";
-
-const router = express.Router();
-
-router.post("/tmp/sum/:a/:b", getSum);
-
-export default router;
