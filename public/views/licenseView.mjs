fae156533b22d3e6d8ff2a1dd12d4394f0fba81a
Guro <guro.stromnes@gmail.com>
2025-03-02 15:23:35 +0100

    Lagt inn view av lisenser og lagt inn lisens til logo brukt til appen

---
+++ public/views/licenseView.mjs
@@ -0,0 +1,9 @@
+
+const licenseViewHTML = `
+<h2>Lisenser</h2>
+<br>
+<h3>Lisens til appens logo</h3>
+<p>Logo laget av <a href="https://thenounproject.com/icon/knitting-7435438/" title="Color Combo">Color Combo</a> fra <a href="https://thenounproject.com/" title="The Noun Project">www.thenounproject.com</a> med ID #7435438</p>
+`;
+
+export default licenseViewHTML;
