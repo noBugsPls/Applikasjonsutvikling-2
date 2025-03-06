c536ed84d8e108d0b05d791fa92b6b3d03f5e83e
Guro <guro.stromnes@gmail.com>
2025-03-06 16:17:40 +0100

    Importerer datastruktur istedenfor å duplisere kode.

---
+++ data/knittingPatternsDatastructure.mjs
@@ -0,0 +1,41 @@
+const knittingPatterns = [
+  {
+    name: "Genser med fletter",
+    description: "En varm og myk ullgenser med flettemønster.",
+    difficulty: "Middels",
+    category: "Genser",
+    sizes: ["S", "M", "L", "XL"],
+    chestWidthInCm: [90, 100, 110, 120],
+    gauge: { stitches: 18, lengthInCm: 10 },
+    materials: [
+      {
+        type: "Garn",
+        name: "Dale lerke",
+        amount: "350g",
+      },
+      {
+        type: "Strikkepinner",
+        name: "Rundpinne",
+        size: "4.5mm",
+      },
+    ],
+    instructions: [
+      { part: "neckline", description: "Legg opp 81 masker og strikk vrangbord i 5 cm." },
+      {
+        part: "body",
+        description:
+          "Strikk glattstrikk til arbeidet måler 40 cm. Sett av 20 masker til ermer på hver side. Strikk til arbeidet måler 60 cm og begynn på flette mønster. Flettene strikkes over 12 masker. Gjenta flettemønsteret 5 ganger. Strikk til arbeidet måler 70 cm og fell av.",
+      },
+      {
+        part: "sleeves",
+        description:
+          "Ta opp de 20 maskene på hver side og strikk ermer i glattstrikk til ermet måler 20 cm. Fell av og fest alle tråder.",
+      },
+    ],
+    image:
+      "https://www.strikkia.no/media/catalog/product/cache/fbf7815f8fb475bd5fe62b6d2d73b520/d/l/dlg-23-052-3043c-fredrikkes-flettegenser-lysegraa-001.jpg",
+    author: "Strikkemor",
+  },
+];
+
+export default knittingPatterns;
