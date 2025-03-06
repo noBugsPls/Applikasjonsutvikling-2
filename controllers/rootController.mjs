136d2111f0f76c26c80a0284cfa32259c5a1e262
Guro <guro.stromnes@gmail.com>
2025-03-06 16:16:33 +0100

    Fjernet elementer som ikke brukes

--- controllers/rootController.mjs
+++
@@ -1,5 +0,0 @@
-import HTTP_CODES from "../utils/httpCodes.mjs";
-
-export function getRoot(req, res, next) {
-    res.status(HTTP_CODES.SUCCESS.OK).send("Hello World").end();
-  };
--- routes/rootRoutes.mjs
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getRoot } from "../controllers/rootController.mjs";
-
-const router = express.Router();
-
-router.get("/", getRoot);
-
-export default router;
