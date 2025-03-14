f570212ae9ab334e12e1b96d43855f1268ff0350
Guro <guro.stromnes@gmail.com>
2025-03-14 04:53:37 +0100

    *Endret mappestruktur* Lagt inn apiservice-lag

---
+++ public/script/apiService.mjs
@@ -0,0 +1,51 @@
+"use strict";
+
+export async function getPatterns() {
+  try {
+    const response = await fetch("/patterns");
+    if (!response.ok) throw new Error("Error fetching patterns");
+    return await response.json();
+  } catch (error) {
+    console.error("Error in getPatterns: ", error);
+  }
+}
+
+export async function createPattern(newPatternData) {
+   try {
+    const response = await fetch("/patterns", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newPatternData),
+    });
+    if (!response.ok) {
+        console.log("response", response);
+      throw new Error(`Error creating pattern: ${response.status} ${response.statusText}`);
+    }
+    const created = await response.json();
+    console.log("created", created);
+    return created;
+  } catch (error) {
+    console.error("Error creating pattern", error);
+    throw error;
+  }
+}
+
+
+export async function deletePattern(id) {
+  try {
+    const response = await fetch(`/patterns/${id}`, {
+      method: "DELETE",
+    });
+    console.log("response", response);
+    if (!response.ok) {
+      console.error("Error deleting pattern", response.status);
+      return;
+    }
+    getPatterns();
+    return await response.json();
+  } catch (error) {
+    res.status(HTTP_CODES.SERVER_ERROR.INTERNAL_SERVER_ERROR)
+   .json({ error: "Error creating pattern", details: error.message });
+
+  }
+}
