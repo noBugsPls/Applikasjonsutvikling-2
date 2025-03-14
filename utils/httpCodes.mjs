660a7c70675fd6b4eefd9bd410cce4afad348ac4
Guro <guro.stromnes@gmail.com>
2025-03-14 04:43:46 +0100

    Lagt til flere HTTP-koder

--- utils/httpCodes.mjs
+++ utils/httpCodes.mjs
@@ -1,17 +1,16 @@
-
-
 const HTTP_CODES = {
-
-    SUCCESS: {
-        OK: 200
-    },
-    CLIENT_ERROR: {
-        BAD_REQUEST: 400,
-        NOT_FOUND: 404
-    },
-    SERVER_ERROR: {
-        INTERNAL_SERVER_ERROR: 500
-    }
+  SUCCESS: {
+    OK: 200,
+    CREATED: 201,
+    NO_CONTENT: 204,
+  },
+  CLIENT_ERROR: {
+    BAD_REQUEST: 400,
+    NOT_FOUND: 404,
+  },
+  SERVER_ERROR: {
+    INTERNAL_SERVER_ERROR: 500,
+  },
 };
 
 export default HTTP_CODES;
