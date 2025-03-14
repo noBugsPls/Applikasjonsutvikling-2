d8856fc5d8add05c29a756394a00b8962467b5ab
Guro <guro.stromnes@gmail.com>
2025-03-14 04:41:04 +0100

    Slettet filer på grunn av rydding i kode og endret mappestruktur

--- public/views/homeView.mjs
+++
@@ -1,8 +0,0 @@
-const homeViewHTML = `
-<h1>Knitting Patterns</h1>
-<p>Et sted hvor du kan oppbevare alle strikkeoppskriftene dine!</p>
-<button id="newPattern">Legg til ny oppskrift</button>
-<button id="showPatterns">Vis oppskrifter</button>
-`;
-
-export default homeViewHTML;
